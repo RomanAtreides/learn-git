d547a4925a402e30dcf98b21cd6eabbc18cb8bbe
Roman Nazarenko <justaspeckinspace@gmail.com>
2022-07-12 20:14:48 -0400

    feat: add 3 new file

---
+++ two_functions.py
@@ -0,0 +1,14 @@
+# Tony Gaddis
+# Programm 5.2
+
+def main():
+	print('I have a news for you')
+	message()
+	print('Goodbye')
+
+def message():
+	print('Today is a good day')
+	print('Today is Sunday')
+
+main()
+
